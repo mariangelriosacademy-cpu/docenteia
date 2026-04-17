12b29c9a7c0d62801d5716b744e509de2aaa2db4
Mariangel Michell <mariangelrios.academy@gmail.com>
2026-04-17 15:22:32 -0500

    Proyecto base Next.js funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
-app/page.tsx
+export default function Home() {
+  return (
+    <main className="p-10 bg-gray-100 min-h-screen">
+      <h1 className="text-6xl font-extrabold text-red-600">
+        ClassMind 🧠
+      </h1>
+
+      <p className="text-gray-600 mt-2">
+        Plataforma educativa con inteligencia artificial
+      </p>
+    </main>
+  );
+}
